d5318a43c63390f35d2a2960d32015ea66157c9c
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-07-27 12:07:50 +0530

    added comment

--- src/main/java/com/pdev/user_service/constant/RolePermissionsConstants.java
+++ src/main/java/com/pdev/user_service/constant/RolePermissionsConstants.java
@@ -8,5 +8,5 @@ public class RolePermissionsConstants {
 
     public static final String CLIENT_ID_USER_AUTH = "user-auth-client";
     public static final String PERMISSION_USER_AUTH_SERVICE = "user-service-permission";
-    public static final String APP_SCOPE_USER_AUTH_SERVICE = "pdev-user";
+    public static final String APP_SCOPE_USER_AUTH_SERVICE = "pdev_user";
 }
